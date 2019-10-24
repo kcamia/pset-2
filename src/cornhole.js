f1857cb93565b9585e657af9eb0b005292676eb7
Kristina Camia <kcamia19@gmail.com>
2019-10-24 13:06:54 -0400

    cornhole

---
+++ src/cornhole.js
@@ -0,0 +1,18 @@
+const readlineSync = require("readline-sync");
+
+const length = 48
+const width = 24
+const diameter = 6
+
+let radius = diameter / 2
+let squared = 2
+let radius2 = radius ** squared
+
+let circleArea = 3.1415 * radius2
+
+let boardArea = length * width
+
+let surfaceArea = boardArea - circleArea
+let area = surfaceArea.toLocaleString("en",{minimumFractionDigits: 2, maxiumumFractionDigits: 2});
+
+console.log("\nThe surface area of a standard Cornhole board is " + area + " square inch(es).")
