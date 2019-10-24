324f0ad72db198abb18b6ce3d57c9b4b8190ba77
Kristina Camia <kcamia19@gmail.com>
2019-10-24 13:01:42 -0400

    diagonal

--- src/diagonal.js
+++ src/diagonal.js
@@ -1,12 +1,12 @@
 const readlineSync = require("readline-sync");
 
-let width = readlineSync.question("Width: ");
+let width = readlineSync.question("\nWidth: ");
 let length = readlineSync.question("Length: ");
 let squared = 2
 let a2 = width ** squared
 let b2 = length ** squared
 let c2 = a2 + b2
-let c = math.sqrt(c2)
-let d = c.toFixed(2);
+let c = Math.sqrt(c2)
+let end = c.toFixed(2)
 
-console.log("A(n) " + width + "-by-" + length + "-inch sheet of paper has a diagonal of " + d + " inch(es).")
+console.log("\nA(n) " + width + "-by-" + length + "-inch sheet of paper has a diagonal of " + end + " inch(es).")
